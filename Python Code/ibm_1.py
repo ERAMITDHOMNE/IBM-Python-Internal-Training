a446cdff7912d97fd3039d9084858ee1fb9ce42f
AMITDHOMNE <48544644+ERAMITDHOMNE@users.noreply.github.com>
2023-06-21 23:52:19 +0530

    Python Program
    How to run python code on command prompt

---
+++ Python Code/ibm_1.py
@@ -0,0 +1,6 @@
+# Program by Amit
+x1 = 1   # data
+x2 = 3
+x3 = x1+x2 # Addition of two variable
+print(x3) # print 
+
---
+++ Python Code/ibm_2.py
@@ -0,0 +1,5 @@
+# Program by Amit
+x1 = float(input("Enter the first number :"))
+x2 = float(input("Enter the second number:" ))
+x3 = x1 * x2
+print(x3)
